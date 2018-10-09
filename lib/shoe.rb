cf050326044bdde2427ac9058406677922d87211
Peter Kreppein <pmkreppein@gmail.com>
2018-10-09 20:51:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,17 +4,15 @@ class Shoe
   BRANDS = Array.new
   def initialize(brand)
     @brand = brand
+     if !BRANDS.include(brand)
+      BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  def brand=(brand)
-    @brand = brand
-    if !BRANDS.include(brand)
-      BRANDS << brand
-    end
+  
   end
 
 end
