3de7750842c7a8d22992dc00647f49ca775ce63d
Peter Kreppein <pmkreppein@gmail.com>
2018-10-09 20:52:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = Array.new
+  
   def initialize(brand)
     @brand = brand
      if !BRANDS.include(brand)
