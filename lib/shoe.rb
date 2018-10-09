f06c68b5adb5be5982ff230a916330ab02e4dbea
Peter Kreppein <pmkreppein@gmail.com>
2018-10-09 20:54:20 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-   
-      BRANDS << brand unless BRANDS.include?(brand)
+   BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
@@ -15,4 +14,3 @@ class Shoe
   end
   
   end
-end
